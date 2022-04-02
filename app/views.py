d08b32639b5c05300f95889877e5539e36401156
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-04-03 01:57:34 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -455,11 +455,10 @@ def editpersonalcarinfoPH(request,owner,car_vin):
                 string = str(e)
                 message = string
                 if 'update or delete on table "listings" violates foreign key constraint "unavailable_owner_car_vin_fkey" on table "unavailable"' in string:  
-                    message = 'There is data linked to this email in unavailable table!' 
+                    message = 'Error! There is data in the unavailable table linked to this email!' 
                 messages.error(request, message)
                 return render(request, "app/editpersonalcarinfoPH.html")
             return redirect('editpersonalcarinfoPH') ##### i added this so it routes to unavailablecarinfo.html after 
-    status = 'Listing edited successfully!'
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM listings WHERE owner = %s AND car_vin = %s", [owner,car_vin])
         obj = cursor.fetchone()
