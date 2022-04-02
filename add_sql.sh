17af049927f2581d460ada28fede0db24559e256
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-04-02 22:19:03 +0800

    Update add_sql.sh

--- add_sql.sh
+++ add_sql.sh
@@ -28,8 +28,7 @@ done < ".env"
 URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Run the scripts to insert data.
+psql ${URI} -f sql/carshareClean.sql
 psql ${URI} -f sql/AppStoreClean.sql
-psql ${URI} -f sql/AppStoreSchema.sql
-psql ${URI} -f sql/AppStoreCustomers.sql
-psql ${URI} -f sql/AppStoreGames.sql
-psql ${URI} -f sql/AppStoreDownloads.sql
+psql ${URI} -f sql/carshareSchema.sql
+psql ${URI} -f sql/populate.sql
