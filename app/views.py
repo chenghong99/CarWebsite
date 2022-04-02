ff9ab589b5bb55373064fffc32152035543db61b
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-04-03 02:49:00 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -625,12 +625,12 @@ def editrentalcarinfoPH(request,car_vin, pick_up): #<input type="hidden" name="c
                 message = string
                 if 'insert or update on table "rentals" violates foreign key constraint "rentals_owner_fkey"' in string:  
                     message = 'Error! This owner does not exist!'
-                if 'insert or update on table "rentals" violates foreign key constraint "rentals_renter_fkey"' in string:  
+                elif 'insert or update on table "rentals" violates foreign key constraint "rentals_renter_fkey"' in string:  
                     message = 'Error! This renter does not exist!'
-                if 'value too long for type character varying(17)' in string:  
-                    message = 'Error! Please check the Car VIN again!'
-                if 'new row for relation "rentals" violates check constraint "chk_date"' in string:  
+                elif 'new row for relation "rentals" violates check constraint "chk_date"' in string:  
                     message = 'Error! How can you drop off the car before you even pick it up?!'
+                elif 'value too long for type character varying(17)' in string:  
+                    message = 'Error! Please check the Car VIN again!'
                 messages.error(request, message)
                 return render(request, "app/editrentalcarinfoPH.html")
             return redirect('editrentalcarinfoPH')
