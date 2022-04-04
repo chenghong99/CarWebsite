b5227447dad319114527cbf1040575b97f1abe70
agnhannah <72562309+agnhannah@users.noreply.github.com>
2022-04-05 05:01:04 +0800

    url test

--- app/views.py
+++ app/views.py
@@ -731,14 +731,13 @@ def search(request):
         if max_rate:
             filter_dict['max_rate'] = max_rate
         
-        #filters_id = (carmakes_id+'/'+models_id+'/'+str(max_year if max_year else 0)+'/'+str(max_mileage if max_mileage else 0)+'/'+str(min_rate if min_rate else 0)+'/'+str(max_rate if max_rate else 0))
-        #filter_dict['filters_id'] = filters_id
-        return redirect(search_results,pick_up,drop_off,filter_dict)
+        filters_id = (carmakes_id+'/'+models_id+'/'+str(max_year if max_year else 0)+'/'+str(max_mileage if max_mileage else 0)+'/'+str(min_rate if min_rate else 0)+'/'+str(max_rate if max_rate else 0))
+        return redirect(search_results,pick_up,drop_off,filters_id)
     return render(request,'app/search.html',filter_dict)
 
 
 #Hannah
-def search_results(request,pick_up,drop_off,filter_dict):
+def search_results(request,pick_up,drop_off,filters_id):
     with connection.cursor() as cursor:
         result_dict = {}
         cursor.execute("SELECT * FROM listings l \
