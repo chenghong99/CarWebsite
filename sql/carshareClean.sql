1b81d42e379d011be4bead2de69023369354a467
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-04-05 02:22:11 +0800

    Create carshareClean.sql

---
+++ sql/carshareClean.sql
@@ -0,0 +1,8 @@
+/*******************
+  Cleaning script
+*******************/
+
+DROP TABLE IF EXISTS rentals;
+DROP TABLE IF EXISTS unavailable;
+DROP TABLE IF EXISTS listings;
+DROP TABLE IF EXISTS customer;
