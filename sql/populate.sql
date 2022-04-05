ae26687753103906961842e5190f146b18d44fe5
agnhannah <72562309+agnhannah@users.noreply.github.com>
2022-04-05 15:26:41 +0800

    Update populate.sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -11,23 +11,42 @@ insert into customer (first_name, last_name, username, dob, password, confirmpas
 insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Amy', 'Warry', 'awarry9', '1/11/2002', 'ZZvye03xvN8', 'ZZvye03xvN8', 'awarry9@statcounter.com',96539517);
 insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('cheng', 'h', 'ch1234', '1/12/2002', 'nus', 'nus', 'chenghong1234@gmail.com',92239517);
 insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('c', 'hong', 'ch123', '1/12/2002', 'nus', 'nus', 'chenghong123@gmail.com',93339517);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Cordy', 'Scourge', 'cscourge0', '1963-05-19', 'yXNclyvUow', 'yXNclyvUow', 'cscourge0@cyberchimps.com', 88830983);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Mike', 'Sigge', 'msigge1', '1954-01-16', 'sx12lcg', 'sx12lcg', 'msigge1@drupal.org', 80683064);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Kennan', 'Levay', 'klevay2', '1978-11-05', 'qbUTOmR2', 'qbUTOmR2', 'klevay2@google.es', 98431739);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Marlowe', 'Wagstaff', 'mwagstaff3', '1948-08-28', 'Mq94tTVGv4', 'Mq94tTVGv4', 'mwagstaff3@icio.us', 86877197);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Hugo', 'Oag', 'hoag4', '1956-08-19', 'rZMqrL9cQq', 'rZMqrL9cQq', 'hoag4@amazon.de', 85658151);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Wilmette', 'Kinchlea', 'wkinchlea5', '1975-02-04', '3xJiDnj', '3xJiDnj', 'wkinchlea5@washingtonpost.com', 80616484);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Bud', 'Brandone', 'bbrandone6', '1959-05-10', 'nvcMnW', 'nvcMnW', 'bbrandone6@t.co', 91124782);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Sinclare', 'Harriday', 'sharriday7', '1997-02-19', 'uwwiQ8vcyn', 'uwwiQ8vcyn', 'sharriday7@weather.com', 89047548);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Constancy', 'Aubin', 'caubin8', '1970-07-01', 'ZWUXBk', 'ZWUXBk', 'caubin8@imgur.com', 81907170);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Marthe', 'Barrowclough', 'mbarrowclough9', '1997-11-05', 'xqOrBns3qYJT', 'xqOrBns3qYJT', 'mbarrowclough9@vimeo.com', 92151831);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Rik', 'Carlyon', 'rcarlyona', '1971-08-19', 'kN35hyJ6xxkV', 'kN35hyJ6xxkV', 'rcarlyona@xinhuanet.com', 95206084);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Blondy', 'Adamovicz', 'badamoviczb', '1952-02-16', '7BZLOMO', '7BZLOMO', 'badamoviczb@omniture.com', 98048417);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Vicki', 'Kruger', 'vkrugerc', '1996-02-22', 'QPGjZW', 'QPGjZW', 'vkrugerc@economist.com', 89337226);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Pam', 'Fennessy', 'pfennessyd', '1952-05-23', 'oCBQ2UhE', 'oCBQ2UhE', 'pfennessyd@discuz.net', 88994982);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Ivy', 'La Batie', 'ilabatiee', '1974-01-14', 'AR5jcM3To1tD', 'AR5jcM3To1tD', 'ilabatiee@chron.com', 92746837);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Devan', 'Pargiter', 'dpargiterf', '1980-03-03', 'Nf5EONBd', 'Nf5EONBd', 'dpargiterf@macromedia.com', 95456695);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Lyon', 'Ebbage', 'lebbageg', '1991-09-10', '0EdeSaeRtJ', '0EdeSaeRtJ', 'lebbageg@unc.edu', 87793141);
+insert into customer (first_name, last_name, username, dob, password, confirmpassword, email, mobile_number) values ('Zahara', 'Besse', 'zbesseh', '1995-04-22', 'vnLo4Mw', 'vnLo4Mw', 'zbesseh@sourceforge.net', 99187026);
 
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('3N1AB6AP3AL362912', 'Isuzu', 'Ascender', 2007, 80643, 50026, 'ksprulls4@fda.gov');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5UMDU93557L734848', 'Volvo', 'XC70', 2007, 51820, 19742, 'ksprulls4@fda.gov');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('1N6AD0CU3FN047703', 'Nissan', 'Titan', 2004, 78648, 53456, 'adimond5@twitpic.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('3FADP0L3XBR853159', 'Dodge', 'Ram 50', 1993, 38823, 16470, 'jgailor7@rediff.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AR2MM8FC452719', 'Subaru', 'Tribeca', 2011, 70290, 35423, 'jgailor7@rediff.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('WAUSFAFL2BA782648', 'Volkswagen', 'Cabriolet', 2002, 34712, 86578, 'sbompass8@cnet.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('WA1LMBFE7ED541878', 'Hyundai', 'Elantra', 2003, 44723, 23456, 'sbompass8@cnet.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('JN8AS1MU7AM855309', 'Chrysler', 'Town & Country', 1993, 47443, 35895, 'sbompass8@cnet.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AN0NU1CN694265', 'Chevrolet', 'Suburban 1500', 1995, 19175, 45717, 'awarry9@statcounter.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('JN8AZ2KRXBT327366', 'Audi', 'TT', 2011, 59961, 62219, 'awarry9@statcounter.com');
-insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AN0NU1CN694264', 'Audi', 'TT', 2013, 59961, 62219, 'chenghong123@gmail.com');
+
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('3N1AB6AP3AL362912', 'Isuzu', 'Ascender', 2007, 80643, 55, 'ksprulls4@fda.gov');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5UMDU93557L734848', 'Volvo', 'XC70', 2007, 51820, 78, 'ksprulls4@fda.gov');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('1N6AD0CU3FN047703', 'Nissan', 'Titan', 2004, 78648, 71, 'adimond5@twitpic.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('3FADP0L3XBR853159', 'Dodge', 'Ram 50', 1993, 38823, 165, 'jgailor7@rediff.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AR2MM8FC452719', 'Subaru', 'Tribeca', 2011, 70290, 199, 'jgailor7@rediff.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('WAUSFAFL2BA782648', 'Volkswagen', 'Cabriolet', 2002, 34712, 130, 'sbompass8@cnet.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('WA1LMBFE7ED541878', 'Hyundai', 'Elantra', 2003, 44723, 45, 'sbompass8@cnet.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('JN8AS1MU7AM855309', 'Chrysler', 'Town & Country', 1993, 47443, 62, 'sbompass8@cnet.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AN0NU1CN694265', 'Chevrolet', 'Suburban 1500', 1995, 19175, 144, 'awarry9@statcounter.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('JN8AZ2KRXBT327366', 'Audi', 'TT', 2011, 59961, 83, 'awarry9@statcounter.com');
+insert into listings (car_vin, carmake, model, year, mileage, rate, owner) values ('5N1AN0NU1CN694264', 'Audi', 'TT', 2013, 59961, 120, 'chenghong123@gmail.com');
 
 insert into unavailable (car_vin, owner, unavailable) values ('3N1AB6AP3AL362912', 'ksprulls4@fda.gov', '2022-12-14');
 insert into unavailable (car_vin, owner, unavailable) values ('3N1AB6AP3AL362912', 'ksprulls4@fda.gov', '2023-01-24');
 insert into unavailable (car_vin, owner, unavailable) values ('3N1AB6AP3AL362912', 'ksprulls4@fda.gov', '2023-01-10');
-insert into unavailable (car_vin, owner, unavailable) values ('JN8AZ2KRXBT327366', 'awarry9@statcounter.com', '02/03/2023');
+insert into unavailable (car_vin, owner, unavailable) values ('JN8AZ2KRXBT327366', 'awarry9@statcounter.com', '2022-03-02');
 insert into unavailable (car_vin, owner, unavailable) values ('JN8AZ2KRXBT327366', 'awarry9@statcounter.com', '2022-09-22');
 insert into unavailable (car_vin, owner, unavailable) values ('5N1AN0NU1CN694265', 'awarry9@statcounter.com', '2022-09-23');
 insert into unavailable (car_vin, owner, unavailable) values ('5N1AN0NU1CN694265', 'awarry9@statcounter.com', '2022-08-04');
@@ -37,9 +56,9 @@ insert into unavailable (car_vin, owner, unavailable) values ('WAUSFAFL2BA782648
 
 insert into rentals (owner,renter,car_vin,pick_up,drop_off,rental_fee)
 values
-('awarry9@statcounter.com','ksprulls4@fda.gov','JN8AZ2KRXBT327366','2022-09-17','2022-09-17',62219),
-('sbompass8@cnet.com','ksprulls4@fda.gov','JN8AS1MU7AM855309','2022-09-17','2022-09-17',35895),
-('jgailor7@rediff.com','ksprulls4@fda.gov','5N1AR2MM8FC452719','2022-09-17','2022-09-17',35423);
+('awarry9@statcounter.com','ksprulls4@fda.gov','JN8AZ2KRXBT327366','2022-09-17','2022-09-17',83),
+('sbompass8@cnet.com','ksprulls4@fda.gov','JN8AS1MU7AM855309','2022-09-17','2022-09-17',62),
+('jgailor7@rediff.com','ksprulls4@fda.gov','5N1AR2MM8FC452719','2022-09-17','2022-09-17',199);
 
 
 insert into rentals (owner,renter,car_vin,pick_up,drop_off,rental_fee)
