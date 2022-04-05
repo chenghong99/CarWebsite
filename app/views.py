f8030cc94041741792c06ed680fbc670779cb819
agnhannah <72562309+agnhannah@users.noreply.github.com>
2022-04-06 00:35:51 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -737,7 +737,7 @@ def search(request):
             max_rate = 'x'
         
 	if max_rate != 'x' and min_rate != 'x':
-            if max_rate < min_rate:
+            if int(max_rate) < int(min_rate):
                 messages.error(request,"Max rate must be more than or equal to min rate")
                 return render(request,'app/search.html')
         return redirect('search_results',pick_up,drop_off,max_year,max_mileage,min_rate,max_rate,carmakes_id,models_id)
